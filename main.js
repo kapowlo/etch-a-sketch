16a8b14968e61b21937680e3c9ddf686eb639cb4
mano <rsmike91@outlook.com>
2023-06-05 23:53:02 -0400

    Made a function that creates a grid 16 by 16 the container now has a certain amount of col and rows based on whatever value I pass in func call
    Added a for loop inside of the makeGrid func
    this for loop creates many div elements inside the container
    the amount of div created is col * rows

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 
+/* 
 
-console.log("hello testing!")
+*/
+
+const myContainer=document.querySelector(".container");
+
+function makeGrid(columns,rows){
+    // select container and create rows and columns
+    myContainer.style.gridTemplateColumns =`repeat(${columns}, 1fr)`; //makes columns based on param value
+    myContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`;//makes rows based on param value
+   
+    for(let i=0;i<columns*rows;i++){ // makes 256 cells
+        let gridCells=document.createElement("div");
+        gridCells.textContent=i + 1; // make sure we have the correct amount,will remove later
+        myContainer.appendChild(gridCells).classList.add("grid-items");
+   }
+   
+}
+makeGrid(16,16);
