2c80a956dc9fa0fd3ac33c5991e0661f76c4c003
mano <rsmike91@outlook.com>
2023-06-07 02:06:38 -0400

    Added a click event on the reset button when I click on the button it will call a function the func resetGridSize() will remove the current grid prompt the user for an input and make a new grid based on user input couple conditional test to make sure user input is valid and not too big

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 
 /* 
-Set up a “hover” effect so that the grid divs change color when your mouse passes over them, 
-leaving a (pixelated) trail through your grid like a pen would.
+
 */
 
 const myContainer=document.querySelector(".container");
+const resetButton=document.querySelector(".btn-reset")
 
 function makeGrid(columns,rows){
-    // select container and create rows and columns
+    // create amount of rows and columns based on param values
     myContainer.style.gridTemplateColumns =`repeat(${columns}, 1fr)`; //makes columns based on param value
     myContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`;//makes rows based on param value
    
-    for(let i=0;i<columns*rows;i++){ // makes 256 cells
+    for(let i=0;i<columns*rows;i++){ // if param 1=16 and param 2 =16 this for loop will create 256cells
         let gridCells=document.createElement("div");
-        gridCells.textContent=i + 1; // make sure we have the correct amount,will remove later
+        //gridCells.textContent=i + 1; // make sure we have the correct amount,will remove later
         myContainer.appendChild(gridCells).classList.add("grid-items");
         
         gridCells.addEventListener("mouseover",(cell)=>{ //when user has his mouse hover over a cell 
@@ -23,3 +23,24 @@ function makeGrid(columns,rows){
    
 }
 makeGrid(16,16);
+
+resetButton.addEventListener("click",resetGridSize);
+function resetGridSize(){
+    //remove the grid,then prompts user for new size,if input is valid make new grid otherwise remake default grid 16 by 16
+    myContainer.querySelectorAll(".grid-items").forEach(cell=>{
+        cell.remove();
+    });
+    const askGridSize=Number(prompt("what size do you want the grid to be,anything above 0 and below 100 is valid"))
+    if(askGridSize>100 || askGridSize<=0){ //if user presses ESC/cancel, or enters nothing run this if code block
+        let msg=alert("please enter a valid input...remaking default grid");
+        makeGrid(16,16);
+        return msg;
+    }
+    else if(isNaN(askGridSize)){
+        msg=alert("Letters are not allowed...Numbers only!");
+        return msg;
+    }
+    else{
+        makeGrid(askGridSize,askGridSize);
+    }
+}
