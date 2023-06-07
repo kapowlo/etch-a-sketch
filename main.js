8e09c544ec014254434612c5e04024e80079c15a
mano <rsmike91@outlook.com>
2023-06-07 03:46:18 -0400

    Added two buttons randomColor and BlackColor both buttons have a click event and arrow func which will select all grid items, loop through them and add another event listener, this is time a mouse over event which controls the color of the cells in my grid

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
 
-/* 
 
-*/
+let color="rgb(0,0,255)";//default color is set to blue,when user mouse over grid the bg color will be blue,also after reset color is blue
 
 const myContainer=document.querySelector(".container");
-const resetButton=document.querySelector(".btn-reset")
+const resetButton=document.querySelector(".btn-reset");
 
+// create two buttons randomColor and blackColor 
+const randomColorButton=document.createElement("button");
+randomColorButton.textContent="Random Color";
+randomColorButton.classList.add("btn","btn-random")
+
+const blackColorButton=document.createElement("button");
+blackColorButton.textContent="Black Color";
+blackColorButton.classList.add("btn","btn-black")
+
+// append buttons to container div
+myContainer.append(randomColorButton,blackColorButton);
+
+
+//this func when called makes a grid, the size of the grid is based on user input
+//but only if input is valid otherwise the grid will have 256cells by default 16*16=256
 function makeGrid(columns,rows){
-    // create amount of rows and columns based on param values
     myContainer.style.gridTemplateColumns =`repeat(${columns}, 1fr)`; //makes columns based on param value
     myContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`;//makes rows based on param value
    
-    for(let i=0;i<columns*rows;i++){ // if param 1=16 and param 2 =16 this for loop will create 256cells
+    for(let i=0;i<columns*rows;i++){ // if param 1=16 and param 2=16 this for loop will create 256cells
         let gridCells=document.createElement("div");
-        //gridCells.textContent=i + 1; // make sure we have the correct amount,will remove later
-        myContainer.appendChild(gridCells).classList.add("grid-items");
+        
+        myContainer.appendChild(gridCells).classList.add("grid-items"); // adds all the divs created to container div
         
         gridCells.addEventListener("mouseover",(cell)=>{ //when user has his mouse hover over a cell 
-            cell.target.style.backgroundColor="orange";  //the bg color of that particular cell will change
+            cell.target.style.backgroundColor=color;  //the bg color of that particular cell will change 
         })
    }
    
 }
-makeGrid(16,16);
+makeGrid(16,16); // start with a default grid of 256 cells better user experience to start with a default grid right away.
 
 resetButton.addEventListener("click",resetGridSize);
 function resetGridSize(){
@@ -30,9 +43,12 @@ function resetGridSize(){
     myContainer.querySelectorAll(".grid-items").forEach(cell=>{
         cell.remove();
     });
+
     const askGridSize=Number(prompt("what size do you want the grid to be,anything above 0 and below 100 is valid"))
+   
     if(askGridSize>100 || askGridSize<=0){ //if user presses ESC/cancel, or enters nothing run this if code block
         let msg=alert("please enter a valid input...remaking default grid");
+        color="rgb(0,0,255)" //fixes a bug after clicking on black button and resetting the grid, cell color would be black instead of blue
         makeGrid(16,16);
         return msg;
     }
@@ -41,6 +57,35 @@ function resetGridSize(){
         return msg;
     }
     else{
-        makeGrid(askGridSize,askGridSize);
+        color="rgb(0,0,255)" //fixes a bug after clicking on black button and resetting the grid, cell color would be black instead of blue
+        makeGrid(askGridSize,askGridSize); // make a grid, the size is based on the user input
     }
 }
+
+//after user clicks on this button, when they mouse over a grid cell the bg color will be different shades of white/gray and black
+blackColorButton.addEventListener("click",()=>{
+   myContainer.querySelectorAll(".grid-items").forEach(cell=>{
+    cell.addEventListener("mouseover",()=>{
+        let rgbValue=Math.floor(Math.random()*255)
+        color=`rgb(${rgbValue},${rgbValue},${rgbValue})`;
+        cell.style.backgroundColor=color; //this line fixed a bug where the bg color would not instantly be changed
+    })
+   })
+})
+
+//after user clicks on this button, when they mouse over a grid cell the bg color will be random
+randomColorButton.addEventListener("click",()=>{
+    // select all the elements with class .grid-items loop through each of them and add an eventlistener
+    //use math to generate a random number round that number and multiply by 255
+    myContainer.querySelectorAll(".grid-items").forEach(cell=>{
+        cell.addEventListener("mouseover",()=>{
+            let rValue=Math.floor(Math.random()*255); 
+            let gValue=Math.floor(Math.random()*255); 
+            let bValue=Math.floor(Math.random()*255); 
+            color=`rgb(${rValue},${gValue},${bValue})`
+            cell.style.backgroundColor=color; //this line fixed a bug where the bg color would not instantly be changed
+        })
+       
+    })
+    
+})
